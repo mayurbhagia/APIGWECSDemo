116597dbb75660da525ffa975485dcb965a443e2
Mayur Bhagia <mayurbhagia@gmail.com>
2019-08-01 22:58:21 +0530

    YOUR MESSAGE

---
+++ src/main/java/com/aws/proserve/apigwdemo/APIGWECSDemo/ApigwecsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.proserve.apigwdemo.APIGWECSDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigwecsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigwecsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/proserve/apigwdemo/APIGWECSDemo/ApigwecsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aws.proserve.apigwdemo.APIGWECSDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApigwecsDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
